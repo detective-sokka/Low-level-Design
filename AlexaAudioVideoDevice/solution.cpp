81b7661757751ffa8c40cfc445242e8677e94e40
detective-sokka <saidutt1997@gmail.com>
2025-06-20 18:43:32 -0400

    Refined Alexa Device code with Decorator pattern

--- AlexaAudioVideoDevice/solution.cpp
+++ AlexaAudioVideoDevice/solution.cpp
@@ -1,58 +1,201 @@
+#include <iostream>
+#include <memory>
+
 class Battery
 {
   public:
-    Battery(bool isCharging) : isCharging{isCharging}, percentage{0.0d}
+    Battery() : percentage{0.0}
     {
     }
 
-    virtual void showBatteryPercentage() 
+    double getBatteryPercentage() const
     {
       return percentage;
     }
 
-    void setBatteryPercentage(double percentage)
+    void setBatteryPercentage(double percent)
     {
-      if (doesExist)
-          std::cout << "Percentage is : " << percentage << "and charging status is : " << isCharging;
-      else
-      {
-        std::cout << "Battery not available. " << "and charging status is : " << isCharging;
-      }
+      this->percentage = percent;
     }
+
   private: 
-    bool isCharging;
     double percentage;
 };
 
 class Device
+{    
+  public:
+
+    virtual void showBatteryPercentage() const = 0;   
+    virtual void showFeatures() const = 0;
+
+    virtual bool getChargingStatus() const
+    {
+      return isCharging;
+    }
+
+    virtual void setChargingStatus(bool status)
+    {
+      this->isCharging = status;
+    }
+
+  private: 
+    bool isCharging;
+};
+
+class BasicDevice : public Device
 {
   private:
-    Battery battery;
+    std::shared_ptr<Battery> battery;
+
   public:
-    Device(Battery pBattery) : battery{pBattery}
+    BasicDevice() : battery{nullptr}
+    {      
+    }
+
+    BasicDevice(std::shared_ptr<Battery> pBattery) : battery{pBattery}
     {
     }
 
+    void showFeatures() const override
+    {
+      std::cout << "Basic features engaged." << "\n";
+    }
 
+    void showBatteryPercentage() const override
+    {
+      if (battery)
+        std::cout << "Battery: " << battery->getBatteryPercentage()
+                  << "% | Charging: " << (getChargingStatus() ? "Yes" : "No") << "\n";
+
+      else  
+        std::cout << "Battery not available. " << " Charging status is : " << getChargingStatus() << "\n";
+    }
 };
 
 class AudioFeature
 {
+  // Audio specific interface methods
 };
 
 class ScreenFeature
 {
+  // Screen specific interface methods
 };
 
 
-class AudioDevice : public Device, public AudioFeature
+class AudioDeviceDecorator : public Device, public AudioFeature
 {
+  public:
+    AudioDeviceDecorator(std::shared_ptr<Device> device) : device{device}
+    {
+    }
+
+    void showBatteryPercentage() const override
+    {
+      device->showBatteryPercentage();
+    }
+
+    void showFeatures() const override
+    {
+      this->device->showFeatures();
+      std::cout << "Audio features engaged." << "\n";
+    }
+
+    bool getChargingStatus() const override
+    {
+      return device->getChargingStatus();
+    }
+
+    void setChargingStatus(bool status) override
+    {
+      device->setChargingStatus(status);
+    }
+
+  private:
+    std::shared_ptr<Device> device;
 };
 
-class ScreenDevice : public Decvice, public ScreenFeature
+class ScreenDeviceDecorator : public Device, public ScreenFeature
 {
+  public:
+    ScreenDeviceDecorator(std::shared_ptr<Device> device) : device{device}
+    {
+    }
+
+    void showBatteryPercentage() const override
+    {
+      device->showBatteryPercentage();
+    }
+
+    void showFeatures() const override
+    {
+      this->device->showFeatures();      
+      std::cout << "Screen features engaged." << "\n";
+    }
+
+    bool getChargingStatus() const override
+    {
+      return device->getChargingStatus();
+    }
+
+    void setChargingStatus(bool status) override
+    {
+      device->setChargingStatus(status);
+    }
+
+  private:
+    std::shared_ptr<Device> device;
 };
 
-class AudioScreenDevice : public Device, public AudioFeature, public ScreenFeature
+class AlexaSystem
 {
+  private:
+    AlexaSystem() = default;
+    AlexaSystem(const AlexaSystem&) = delete;
+    AlexaSystem& operator=(const AlexaSystem&) = delete;
+  public:    
+
+    static AlexaSystem& getInstance()
+    {
+      static AlexaSystem instance;
+      return instance;
+    }
+
+    std::shared_ptr<Device> getAudioDevice()
+    {    
+      auto basicDevice = std::make_shared<BasicDevice>();
+      auto audioBasicDevice = std::make_shared<AudioDeviceDecorator>(basicDevice);
+
+      return audioBasicDevice;
+    }
+
+    std::shared_ptr<Device> getScreenDevice()
+    {
+      auto basicDevice = std::make_shared<BasicDevice>();
+      auto screenBasicDevice = std::make_shared<ScreenDeviceDecorator>(basicDevice);
+
+      return screenBasicDevice;
+    }
+
+    std::shared_ptr<Device> getAudioScreenDevice()
+    {
+      auto basicDevice = std::make_shared<BasicDevice>();
+      auto audioBasicDevice = std::make_shared<AudioDeviceDecorator>(basicDevice);
+      auto screenAudioBasicDevice = std::make_shared<ScreenDeviceDecorator>(audioBasicDevice);
+
+      return screenAudioBasicDevice;
+    }
 };
+
+int main()
+{
+  AlexaSystem &instance = AlexaSystem::getInstance();  
+  std::cout << "\nAudio device : \n";
+  instance.getAudioDevice()->showFeatures();
+  std::cout << "\nScreen device : \n";
+  instance.getScreenDevice()->showFeatures();
+  std::cout << "\nAudio Screen device : \n";
+  instance.getAudioScreenDevice()->showFeatures();
+  return 0;
+}
